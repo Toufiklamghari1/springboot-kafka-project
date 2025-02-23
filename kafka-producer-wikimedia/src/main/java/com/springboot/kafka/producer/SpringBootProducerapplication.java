4e45526583e1c6b397a7aec095f1d83fa2055789
Toufik lamghari <64041688+Toufiklamghari1@users.noreply.github.com>
2025-02-23 15:44:35 +0000

    create Kafka producer microservice withing a multimodule project

---
+++ kafka-producer-wikimedia/src/main/java/com/springboot/kafka/producer/SpringBootProducerapplication.java
@@ -0,0 +1,14 @@
+package com.springboot.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @autor Toufik lamghari
+ **/
+@SpringBootApplication
+public class SpringBootProducerapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerapplication.class);
+    }
+}
